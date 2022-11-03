c2270e9fb89de41c759c85d3d84697a2f73ae95c
Coromoo <113568980+tsudaMt@users.noreply.github.com>
2022-11-03 09:39:35 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
-var nowTime = new Date(); //  現在日時を得る
-var nowHour = nowTime.getHours(); // 時を抜き出す
-var nowMin  = nowTime.getMinutes(); // 分を抜き出す
-var nowSec  = nowTime.getSeconds(); // 秒を抜き出す
-var msg = "現在時刻は、" + nowHour + ":" + nowMin + ":" + nowSec + " です。";
-document.getElementById("RealtimeClockArea").innerHTML = msg;
+// デジタル時計表示用
+let clock = null;
+
+// デジタル時計を表示する
+const digitalClock = ()=>{
+    // 現在時刻取得
+    const now = new Date();
+
+    // 時・分・秒取得
+    const [hour, minute, second] = [now.getHours(), now.getMinutes(), now.getSeconds()];
+
+    // 0埋め処理
+    const [txtHour, txtMinute, txtSecond] = [
+        String(hour).padStart(2, "0"),
+        String(minute).padStart(2, "0"),
+        String(second).padStart(2, "0"),
+    ];
+
+    // 表示
+    const txtDigitalClock = `${txtHour}:${txtMinute}:${txtSecond}`;
+    clock.textContent = txtDigitalClock;
+    requestAnimationFrame(digitalClock);
+};
+
+// 起動時の処理
+window.addEventListener("load", ()=>{
+    // 時計表示部分DOM取得
+    clock = document.getElementById("clock");
+
+    // デジタル時計開始
+    digitalClock();
+});
